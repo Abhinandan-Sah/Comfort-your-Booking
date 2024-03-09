8eabcf9968dd28e098ae5e768840abc2d670f561
Abhinandan-Sah <abhinandansah222@gmail.com>
2024-03-09 11:04:09 +0530

    updated

--- app.js
+++ app.js
@@ -2,6 +2,10 @@
 const express = require("express");
 const app = express();
 
+app.get("/", (req, res)=>{
+    res.send("Hi, I am root");
+})
+
 app.listen(8080, ()=>{
     console.log("Listening at port 8080");
 })
---
+++ models/listing.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title: String, 
+    description: String,
+    image: String, 
+    price: Number, 
+    location: String,
+    country: String,
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
