94f0df587149670ac16e7c8d851cbae62b6441f4
Abhinandan-Sah <abhinandansah222@gmail.com>
2024-03-10 00:48:57 +0530

    updated

--- app.js
+++ app.js
@@ -1,11 +1,43 @@
 // console.log("hello");
 const express = require("express");
 const app = express();
+const mongoose = require('mongoose')
+const Listing = require("./models/listing.js")
+
+const MONGO_URL = 'mongodb://127.0.0.1:27017/wanderlust';
+main()
+    .then(() => {
+        console.log("connected to DB");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+
+async function main(){
+    await mongoose.connect(MONGO_URL);
+} 
 
 app.get("/", (req, res)=>{
     res.send("Hi, I am root");
 })
 
-app.listen(8080, ()=>{
+app.get("/testListing", async (req, res) =>{
+    let sampleListing = new Listing ({
+        title: "My New Villa",
+        description: "By the beach",
+        price: 1200,
+        location: "Calangute, Goa",
+        country: "India",
+    });
+
+    await sampleListing.save();
+    console.log("sample was saved");
+    res.send("successful testing");
+})
+
+
+
+app.listen(8080, ()=>{ 
     console.log("Listening at port 8080");
 })
