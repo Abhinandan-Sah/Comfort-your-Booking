c23872d86ad6c8a696c8e737ee12fbf88336e3ba
Abhinandan-Sah <abhinandansah222@gmail.com>
2024-03-20 23:47:08 +0530

    updated

--- app.js
+++ app.js
@@ -1,44 +1,23 @@
-// console.log("hello");
 const express = require("express");
 const app = express();
-const mongoose = require('mongoose')
-const Listing = require("./models/listing.js")
+const mongoose = require("mongoose");
 
-const MONGO_URL = 'mongodb://127.0.0.1:27017/wanderlust';
-main()
-    .then(() => {
-        console.log("connected to DB");
-    })
-    .catch((err) => {
-        console.log(err);
-    });
-
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
 async function main(){
     await mongoose.connect(MONGO_URL);
-} 
+}
 
-app.get("/", (req, res)=>{
-    res.send("Hi, I am root");
+main().then(()=>{
+    console.log("connected to DB");
+}).catch((err)=>{
+    console.log(err);
+});
+
+app.get("/",(req, res) => {
+    res.send("Hello, I am groot");
 })
 
-app.get("/testListing", async (req, res) =>{
-    let sampleListing = new Listing ({
-        title: "My New Villa",
-        description: "By the beach",
-        price: 1200,
-        location: "Calangute, Goa",
-        country: "India",
-    });
-    
-
-    await sampleListing.save();
-    console.log("sample was saved");
-    res.send("successful testing");
-})
-
-
-
-app.listen(8080, ()=>{ 
-    console.log("Listening at port 8080");
-})
+app.listen(8080, ()=>{
+    console.log("server is listening to port 8080");
+});
