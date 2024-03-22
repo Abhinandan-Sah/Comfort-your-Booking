1cc7ed141752ca550eef5638564caff1a8022a7e
Abhinandan-Sah <abhinandansah222@gmail.com>
2024-03-23 02:28:48 +0530

    updated

--- app.js
+++ app.js
@@ -53,7 +53,15 @@ app.get("/listings/:id/edit", async (req, res)=>{
 app.put("/listings/:id", async(req, res)=>{
   let {id} = req.params;
   await Listing.findByIdAndUpdate(id, {...req.body.listing});
-  redirect("/listings");
+  res.redirect(`/listings/${id}`);
+})
+
+//Delete Route
+app.delete("/listings/:id", async (req, res)=>{
+  let {id} = req.params;
+  let deletedListing = await Listing.findByIdAndDelete(id);
+  console.log(deletedListing);
+  res.redirect("/listings");
 })
 
 //Show Route
