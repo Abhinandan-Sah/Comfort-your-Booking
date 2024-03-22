d414475c1866b2595e4703d179ccb49fefd908f7
Abhinandan-Sah <abhinandansah222@gmail.com>
2024-03-22 23:40:44 +0530

    updated

--- app.js
+++ app.js
@@ -6,57 +6,59 @@ const path = require("path");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
-async function main(){
-    await mongoose.connect(MONGO_URL);
+async function main() {
+  await mongoose.connect(MONGO_URL);
 }
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-main().then(()=>{
+main()
+  .then(() => {
     console.log("connected to DB");
-}).catch((err)=>{
+  })
+  .catch((err) => {
     console.log(err);
+  });
+
+app.get("/", (req, res) => {
+  res.send("Hello, I am groot");
 });
 
-app.get("/",(req, res) => {
-    res.send("Hello, I am groot");
-})
+//New Route
+app.get("/listing/new", (req, res)=>{
+    res.render("listing/new.ejs");
+});
 
+//Show Route
+app.get("/listings/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    if (!mongoose.isValidObjectId(id)) {
+      return res.status(400).send("Invalid ObjectId.");
+    }
+
+    const objectId = new mongoose.Types.ObjectId(id);
+    const listing = await Listing.findOne({ _id: objectId });
+
+    if (!listing) {
+      return res.status(404).send("Listing not found.");
+    }
+
+    res.render("listings/show.ejs", { listing });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal server error.");
+  }
+});
 
 //Index Route
 app.get("/listings", async (req, res) => {
-    const allListings = await Listing.find({});
-    res.render("listings/index.ejs", {allListings});
-  });
-
-//   Show Route
-//Show Route
-app.get("/listings/:id", async (req, res) => {
-
-    try {
-        const { id } = req.params;
-    
-        if (!mongoose.isValidObjectId(id)) {
-          return res.status(400).send("Invalid ObjectId.");
-        }
-    
-        const objectId = new mongoose.Types.ObjectId(id);
-        const listing = await Listing.findOne({ _id: objectId });
-    
-        if (!listing) {
-          return res.status(404).send("Listing not found.");
-        }
-    
-        res.render("listings/show.ejs", { listing });
-      } catch (error) {
-        console.error(error);
-        res.status(500).send("Internal server error.");
-      }
-
-  });
-  
+  const allListings = await Listing.find({});
+  res.render("listings/index.ejs", { allListings });
+});
 
 // app.get("/testListing", async (req, res)=>{
 //     let sampleListing = new Listing({
@@ -72,6 +74,6 @@ app.get("/listings/:id", async (req, res) => {
 //     res.send("Successful Testing");
 // })
 
-app.listen(8080, ()=>{
-    console.log("server is listening to port 8080");
+app.listen(8080, () => {
+  console.log("server is listening to port 8080");
 });
--- models/listing.js
+++ models/listing.js
@@ -3,10 +3,10 @@ const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
   title: {
-    type: Object,
+    type: String,
     required: true,
   },
-  description: Object,
+  description: String,
   image: {
     type: Object,
     default:
@@ -16,9 +16,9 @@ const listingSchema = new Schema({
         ? "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
         : v,
   },
-  price: Object,
-  location: Object,
-  country: Object,
+  price: Number,
+  location: String,
+  country: String,
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
